cee26d462533b5f34dc4df6a0eddb31758a60eac
Will Harris <will.t.harris@protonmail.com>
2020-01-16 16:41:41 -0700

    feat: get styled-components installed and working

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,8 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
-    `gatsby-plugin-tslint`,
+	`gatsby-plugin-tslint`,
+	`gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
+import { createGlobalStyle } from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const GlobalStyle = createGlobalStyle`
+	body {
+		color: ${props => (props.theme === "purple" ? "purple" : "white")};
+	}
+`
+
 const IndexPage = () => (
   <Layout>
+	  <GlobalStyle theme="purple" />
     <SEO title="Home" />
-	<p>Test</p>
+    <p>Test</p>
     <Link to="/about/">Go to page 2</Link>
     <Link to="/contact/">Go to contact page</Link>
   </Layout>
