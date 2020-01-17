70dbf35f42354b75b5fd83788ccb22ae93f0f40a
Will Harris <will.t.harris@protonmail.com>
2020-01-16 20:38:48 -0700

    feat: install and configure tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from "react"
+import logo from "../images/gatsby-icon.png"
 import { Link } from "gatsby"
 import { createGlobalStyle } from "styled-components"
+import '../index.css'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -13,9 +15,24 @@ const GlobalStyle = createGlobalStyle`
 
 const IndexPage = () => (
   <Layout>
-	  <GlobalStyle theme="purple" />
+    <GlobalStyle theme="purple" />
     <SEO title="Home" />
-    <p>Test</p>
+    <div className="container flex flex-col mx-auto px-4 text-center">
+      <p className="font-sans">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+        volutpat ipsum vel pellentesque consequat. Phasellus ut justo quis lacus
+        porta tincidunt. In quis ante at risus porttitor hendrerit eu sed lorem.
+        Suspendisse potenti. Aliquam at purus vel est scelerisque euismod. Nam
+        in mi tellus. Etiam mi turpis, dignissim sit amet pretium quis, posuere
+        a nisl. Aenean convallis nibh at posuere malesuada. Orci varius natoque
+        penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        Maecenas blandit dolor elit, ut laoreet mauris varius ut. Nunc a nisi
+        sed justo faucibus dapibus. Donec rhoncus enim non elit facilisis
+        euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac
+        egestas leo.{" "}
+      </p>
+      <img src={logo} alt="logo" className="mt-12 justify-center" />
+    </div>
     <Link to="/about/">Go to page 2</Link>
     <Link to="/contact/">Go to contact page</Link>
   </Layout>
