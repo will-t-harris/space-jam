7181c57dc0ee050d5c524b85b1eed4b00b0c6edf
Will Harris <will.t.harris@protonmail.com>
2020-01-20 11:46:34 -0700

    style: add oranges, refactor colors to rgb

--- src/shared/variables/_colors.ts
+++ src/shared/variables/_colors.ts
@@ -1,7 +1,9 @@
 const colorVariables = {
   homeBackground: "rgb(49, 37, 42, 0.75)",
-  lightText: "#CD9581",
-  darkText: "#31252A",
+  lightText: "rgb(205, 149, 129)",
+  darkText: "rgb(49, 37, 42)",
+  lightOrange: "rgb(219, 93, 57)",
+  darkOrange: "rgb(135, 54, 48)",
 }
 
 export default colorVariables
