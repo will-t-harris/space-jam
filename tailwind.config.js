dcabd4ba2dc02a38a852645f866b6b13c6d22777
Will Harris <will.t.harris@protonmail.com>
2020-02-04 22:03:08 -0800

    chore: add custom colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    colors: {
+      darkOrange: "#873630",
+      lightOrange: "#DB5D39",
+      darkBrown: "#31252A",
+      primaryText: "#CD9581",
+      primaryBackground: "#50474a",
+    },
+    extend: {},
   },
   variants: {},
-  plugins: []
-}
+  plugins: [],
+};
