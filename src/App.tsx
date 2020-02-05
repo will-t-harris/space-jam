e46e152b7cb4653f064574e5d0e0a544693995a8
Will Harris <will.t.harris@protonmail.com>
2020-02-04 22:03:51 -0800

    feat: move markup into Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import Card from "./components/Card";
 import Clock from "./components/Clock";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <body>
+    <div className="text-center">
+      <Header />
+      <body className="bg-primaryBackground">
         <Card title="Welcome!" paragraph="To the example card!" />
         <Clock />
+        <Button />
       </body>
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => (
+  <header className="bg-darkBrown p-6 shadow-lg">
+    <h1 className="text-darkOrange">
+      <span role="img" aria-label="basketball emoji">
+        🏀
+      </span>{" "}
+      <em>space-jam</em>{" "}
+      <span role="img" aria-label="basketball emoji">
+        🏀
+      </span>
+    </h1>
+  </header>
+);
+
+export default Header;
