6d17cc00c80373292fb1deb6942514cdc96a90f9
Will Harris <will.t.harris@protonmail.com>
2020-02-06 15:39:31 -0800

    feat: get button text from props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,21 @@
 import React, { FunctionComponent, MouseEvent } from "react";
 
+type ButtonProps = {
+  text: string;
+};
+
 const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
   event.preventDefault();
   alert(event.currentTarget.tagName);
 };
 
-const Button: FunctionComponent = ({ children }) => {
+const Button: FunctionComponent<ButtonProps> = ({ text }) => {
   return (
     <button
       className="border border-lightOrange rounded px-4 py-2 my-10 hover:bg-lightOrange"
       onClick={handleClick}
     >
-      Test Button {children}
+      {text}
     </button>
   );
 };
