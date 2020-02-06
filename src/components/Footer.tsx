26b128d3f93cca02bc06eae20e34a3932eb8fcab
Will Harris <will.t.harris@protonmail.com>
2020-02-06 15:39:14 -0800

    feat: define basic footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primaryFooter">
+      <ul className="text-primaryText">
+        <li>Contact</li>
+        <li>About</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
